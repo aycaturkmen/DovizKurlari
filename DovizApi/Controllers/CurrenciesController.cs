c7805efcbe11ff82d726d25b8f8c3e6ee42bb0cc
ayca <ayca_97@hotmail.com>
2021-02-04 19:21:08 +0300

    .NET Core Döviz

---
+++ DovizApi/Controllers/CurrenciesController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DovizApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class CurrenciesController:ControllerBase
+    {
+
+        XElement xelement = XElement.Load("https://www.tcmb.gov.tr/kurlar/today.xml"); //xelement tcmb'den çekilen tüm kur bilgilerini tutar
+
+        [HttpGet]
+        public string Get()
+        {
+
+            XmlDocument xmlDoc = new XmlDocument();
+            string xmlTcmbData = xelement.ToString(); 
+            xmlDoc.LoadXml(xmlTcmbData);
+            string jsonTcmbData = JsonConvert.SerializeXmlNode(xmlDoc);
+            return jsonTcmbData;
+
+        }
+
+        [HttpGet("{secilenDoviz}")]
+        public string Get(string secilenDoviz)
+        {
+
+            IEnumerable<XElement> dovizCinsleri = xelement.Elements(); 
+            string secilenDovizCinsi = "";
+            //Tüm döviz cinsleri taranarak seçilen döviz cinsi bulunur. Döviz cinsi Türkçe veya İngilizce aratılabilir.
+            foreach (var dovizCinsi in dovizCinsleri)
+            {
+                if (dovizCinsi.Element("Isim").Value.Equals(secilenDoviz) || dovizCinsi.Element("CurrencyName").Value.Equals(secilenDoviz) )
+                {
+                    secilenDovizCinsi = dovizCinsi.ToString();
+                }              
+            }
+            return secilenDovizCinsi.ToString();
+        }
+
+        [HttpGet("{altLimit},{ustLimit}")]
+        public string Get(decimal altLimit, decimal ustLimit)
+        {
+
+            IEnumerable<XElement> dovizCinsleri = xelement.Elements();
+            string secilenAraliktakiler = "";
+
+            //Döviz satış fiyatı seçilen aralıklar içerisinde olan döviz cinslerini bulur.
+            foreach (var dovizCinsi in dovizCinsleri)
+            {
+                decimal satis=0;               
+                bool e = dovizCinsi.Element("CurrencyName").ToString().Contains("SDR");
+                if (dovizCinsi.Element("ForexSelling") != null && !e)
+                {
+                    satis = (decimal)dovizCinsi.Element("ForexSelling");
+                }
+
+                if (Decimal.Compare(satis, altLimit)>=0 && Decimal.Compare(satis, ustLimit) <= 0)
+                {
+                    secilenAraliktakiler = secilenAraliktakiler + dovizCinsi.ToString();
+                }      
+            }
+            return secilenAraliktakiler.ToString();
+
+        }
+    }
+}
---
+++ DovizApi/Startup.cs
@@ -0,0 +1,79 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace DovizApi
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Doviz Api Swagger",
+                    Description = "Doviz .Net Core Api Swagger Documentation",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Ayca Turkmen",
+                        Email = "trmenayca@gmail.com"
+                    }
+                });
+
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+
+
+            });  
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Doviz API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
